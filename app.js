82ac5f2f6c04e7ba1030abecf246182626ce9b07
Rabeeh <rbh.rabeeh00@gmail.com>
2025-12-29 09:56:25 +0530

    Initial project setup with MVC structure and DB connection

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 3000
+
+connectDB()
+
+app.get('/',(req,res)=>{
+    res.send("Server is running successfully")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at ${PORT}`)
+})
